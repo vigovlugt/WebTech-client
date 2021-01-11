1c3ff2d92d6d134c10ffc8b8c3911c05db14f5fb
Faas Hoetjes <faashoetjes@gmail.com>
2021-01-11 15:42:18 +0100

    style aangepast profile-page

--- views/ProfilePage.js
+++ views/ProfilePage.js
@@ -7,18 +7,36 @@ export default class ProfilePage extends HTMLElement {
   static style = html`<style>
     .profile-avatar {
       border-radius: 50%;
-      width: 128px;
-      height: 128px;
-      margin-right: 1rem;
+      width: 200px;
+      height: 200px;
+      margin-left: auto;
+      margin-right: auto;
+      display: block;
     }
 
     .profile-header {
-      display: flex;
-      align-items: baseline;
+      margin:0 auto;
     }
 
     .profile-name {
       font-size: 3rem;
+      text-align: center;
+    }
+
+    .sidebar-left {
+      height: 100%;
+      width: 17.5%;
+      position: absolute;
+      background-image: url("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/387eb18b-ab74-4503-a97f-b5e6143eb21d/d2eybzq-986ca967-9887-4244-8deb-4f04d43f47bd.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMzg3ZWIxOGItYWI3NC00NTAzLWE5N2YtYjVlNjE0M2ViMjFkXC9kMmV5YnpxLTk4NmNhOTY3LTk4ODctNDI0NC04ZGViLTRmMDRkNDNmNDdiZC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.aLewA490CmwbPu8V6Gz9c3nVfwYQHZeKiVIMI7dzlBk");
+      left: 0px;
+    }
+
+    .sidebar-right {
+      height: 100%;
+      width: 17.5%;
+      position: absolute;
+      background-image: url("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/387eb18b-ab74-4503-a97f-b5e6143eb21d/d2eybzq-986ca967-9887-4244-8deb-4f04d43f47bd.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMzg3ZWIxOGItYWI3NC00NTAzLWE5N2YtYjVlNjE0M2ViMjFkXC9kMmV5YnpxLTk4NmNhOTY3LTk4ODctNDI0NC04ZGViLTRmMDRkNDNmNDdiZC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.aLewA490CmwbPu8V6Gz9c3nVfwYQHZeKiVIMI7dzlBk");
+      right: 0px;
     }
   </style>`;
 
@@ -35,6 +53,8 @@ export default class ProfilePage extends HTMLElement {
     this.innerHTML = html`
       <div class="container">
         <div class="profile-header">
+          <div class="sidebar-left"></div>
+          <div class="sidebar-right"></div>
           <img
             class="profile-avatar"
             src="${this.spotifyProfile.images[0].url}"
@@ -43,7 +63,9 @@ export default class ProfilePage extends HTMLElement {
           />
           <h1 class="profile-name">${this.spotifyProfile.display_name}</h1>
         </div>
-        <pre style="width:1000px;overflow:hidden;">
+        <div class="profile-statistics">
+        </div>
+        <pre style="margin-left:20%;width:1000px;overflow:hidden;">
 ${JSON.stringify(this.spotifyProfile, null, 2)}
         </pre
         >
